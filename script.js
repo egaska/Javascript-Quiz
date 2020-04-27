f3285f858c3a6157a74231fd287f5e4da48aa49c
Emily Gaska <emilygaska@gmail.com>
2020-04-27 16:36:30 -0500

    Set up basic HTML and CSS skeleton to build Javascript on. Added basic variables and test questions

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 
 // Initialize  Global Variables
 
-var currentQuestion;
-var correctAnswer;
-var wrongAnswer;
+var currentQuestion; 
+var correctAnswer; 
+var incorrectAnswer; 
+
+
+// Questions Array Variable 
+
+var questionsArray =
+    [
+        {
+            question: "Test test test",
+            choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+            answer: 2
+        },
+        {
+            question: "Test test test 2",
+            choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+            answer: 4
+        },
+        {
+            question: "Test test test 3",
+            choices: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+            answer: 1
+        },
+    ];
+
